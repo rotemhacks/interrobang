5ec3fddb97fde0420ff00201f8755721afce191c
Rotem S. <rotemdraws@gmail.com>
2026-01-08 17:32:26 +0900

    add basic archive page

--- src/routes/(main)/archive/+page.svelte
+++ src/routes/(main)/archive/+page.svelte
@@ -6,5 +6,13 @@
 	<p>Start reading from:</p>
 	{#each data.volumes as vol (vol.id)}
 		<div class="bg-primary"><span>{vol.volnum}: {vol.title}</span></div>
+		{#each vol.chapters as chapter (chapter.id)}
+			<div class="bg-secondary"><span>{chapter.chapnum}: {chapter.title}</span></div>
+			<div class="flex flex-row gap-4">
+				{#each chapter.pages as page (page.id)}
+					<img src={page.thumb} alt={page.title} />
+				{/each}
+			</div>
+		{/each}
 	{/each}
 </div>
