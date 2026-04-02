bd00591de8637fae76946ab623bff917cbc62e74
Rotem S. <rotemdraws@gmail.com>
2026-04-02 16:29:39 +0300

    add remote function for archive

--- src/lib/remote/comic.remote.ts
+++ src/lib/remote/comic.remote.ts
@@ -25,6 +25,24 @@ export const getAllPagesWithDetails = query(async () =>
 		.orderBy(desc(volumes.id), desc(chapters.id), desc(pages.pagenum))
 );
 
+export const getAllPagesForArchive = query(async () => {
+	const result = await db.query.volumes.findMany({
+		with: {
+			chapters: {
+				orderBy: (chapters, { asc }) => [asc(chapters.chapnum)],
+				with: {
+					pages: {
+						orderBy: (pages, { asc }) => [asc(pages.pagenum)]
+					}
+				}
+			}
+		}
+	});
+
+	result.sort((a, b) => a.volnum - b.volnum);
+	return result;
+});
+
 export const getPageBySlug = query(z.string(), async (slug) =>
 	db.select().from(pages).where(eq(pages.slug, slug))
 );
