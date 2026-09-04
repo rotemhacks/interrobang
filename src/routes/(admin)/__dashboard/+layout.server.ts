4b4c827e696f7d37feae9d4c1b9cefd3324e3132
Rotem S. <rotemdraws@gmail.com>
2026-09-04 18:51:24 +0300

    change redirect for auth guard

--- src/routes/(admin)/__dashboard/+layout.server.ts
+++ src/routes/(admin)/__dashboard/+layout.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
 	if (locals?.user?.role !== 'admin') {
-		throw redirect(302, '/');
+		throw redirect(302, '/auth/login');
 	}
 	return {};
 };
